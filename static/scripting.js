b4894b99deca9b83793da0bfb215c874b007804f
Matthew Bishop <mattsacommonname@users.noreply.github.com>
2020-09-30 23:16:09 -0700

    Use Marionette.js framework on top of Backbone.js for UI.

--- static/scripting.js
+++ static/scripting.js
@@ -13,231 +13,127 @@
  * limitations under the License.
  */
 
+
+/**
+ * BeagleWizard namespace.
+ */
+var BeagleWizard = {};
+
+
 (function(){
+/**
+ * Application
+ */
+BeagleWizard.App = new Backbone.Marionette.Application();
 
-var BookmarkTemplate = _.template($('#bookmark-template').html());
+BeagleWizard.App.addRegions({
+    bookmarkRegion: "#bookmark-list",
+    tagRegion: "#tag-list"
+});
 
-var BookmarkModel = Backbone.Model.extend({urlRoot: '/b'});
+BeagleWizard.App.addInitializer(function(options){
+  let bookmarkTable = new BeagleWizard.BookmarkTableView({
+    collection: options.bookmarks
+  });
+  BeagleWizard.App.bookmarkRegion.show(bookmarkTable);
 
-var BookmarkCollection = Backbone.Collection.extend({
-    model: BookmarkModel,
+  let tagTable = new BeagleWizard.TagTableView({
+    collection: options.tags
+  });
+  BeagleWizard.App.tagRegion.show(tagTable);
+});
+
+
+/**
+ *
+ */
+BeagleWizard.BookmarkModel = Backbone.Model.extend({});
+
+
+/**
+ *
+ */
+BeagleWizard.BookmarkCollection = Backbone.Collection.extend({
+    model: BeagleWizard.BookmarkModel,
     url: '/b'
 });
 
-var BookmarkView = Backbone.View.extend({
-    model: BookmarkModel,
-    tagName: 'tr',
 
-    /**
-     * Deletes the bookmark represented by the current model.
-     *
-     * @param {jQuery.Event} e The jQuery click event. Contains the model in data.
-     */
-    deleteClick: function(e) {
-        bookmarkId = this.model.get('id');
-        $.ajax({
-            context: this,
-            type: 'DELETE',
-            url: `/b/${bookmarkId}`,
-            error: function(request, status, err) {
-                console.error(`error deleting bookmark ${bookmarkId}: ${err}`);
-            },
-            success: function(data, status, request) {
-                this.remove();
-            }
-        });
+/**
+ *
+ */
+BeagleWizard.BookmarkView = Backbone.Marionette.ItemView.extend({
+    template: "#bookmark-template",
+    tagName: 'tr'
+});
+
+
+/**
+ *
+ */
+BeagleWizard.BookmarkTableView = Backbone.Marionette.CompositeView.extend({
+    tagName: "table",
+    id: "bookmark-table",
+    className: 'is-narrow is-striped table',
+    template: "#bookmark-table-template",
+    itemView: BeagleWizard.BookmarkView,
+
+    initialize: function(){
+        this.listenTo(this.collection, "sort", this.renderCollection);
     },
 
-    /**
-     * Renders the bookmark to the row element it's been assigned.
-     *
-     * @returns {BookmarkView} The current BookmarkView.
-     */
-    render: function() {
-        let bookmarkId = this.model.get('id');
-        let context = {
-            id: bookmarkId,
-            label: this.model.get('label'),
-            text: this.model.get('text'),
-            url: this.model.get('url')
-        };
-        let output = BookmarkTemplate(context);
-        this.$el.html(output);
-        $(`#delete-${bookmarkId}`).click(this, function (e) { e.data.deleteClick(e); });
-        let tagCell = $(`#tags-${bookmarkId}`);
-        let tags = this.model.get('tags');
-        for (tag of tags) {
-            let bookmark_tag_model = new BookmarkTagModel({
-                bookmark_id: bookmarkId,
-                id: tag.id,
-                label: tag.label
-            });
-            let bookmark_tag_view = new BookmarkTagView({ model: bookmark_tag_model });
-            tagCell.append(bookmark_tag_view.$el);
-            bookmark_tag_view.render();
-        }
-        return this;
+    appendHtml: function(collectionView, itemView){
+        collectionView.$('tbody').append(itemView.el);
     }
 });
 
-var BookmarkListView = Backbone.View.extend({
-    model: BookmarkCollection,
 
-    /**
-     * Renders a list of bookmarks to the assigned element.
-     *
-     * @returns {BookmarkListView} This BookmarkListView.
-     */
-    render: function() {
-        this.$el.html('');
-        for (bookmark of this.model) {
-            let bookmark_view = new BookmarkView({model: bookmark});
-            this.$el.append(bookmark_view.$el);
-            bookmark_view.render();
-        }
-        return this;
-    },
+/**
+ *
+ */
+BeagleWizard.TagModel = Backbone.Model.extend({});
 
-    /**
-     * Updates the list view by fetching the collection and rendering it.
-     */
-    update: function() {
-        this.model.fetch({
-            view: this,
 
-            /**
-             */
-            success: function(collection, response, options) {
-                options.view.render();
-            }
-        });
-    }
-});
-
-var BookmarkTagTemplate = _.template($('#bookmark-tag-template').html());
-
-var BookmarkTagModel = Backbone.Model.extend({});
-
-var BookmarkTagView = Backbone.View.extend({
-    model: BookmarkTagModel,
-    tagName: 'span',
-    render: function() {
-        let context = {
-            bookmark_id: this.model.get('bookmark_id'),
-            id: this.model.get('id'),
-            label: this.model.get('label')
-        };
-        let output = BookmarkTagTemplate(context);
-        this.$el.html(output);
-        return this;
-    }
-});
-
-var TagTemplate = _.template($('#tag-template').html());
-
-var TagModel = Backbone.Model.extend({urlRoot: '/t'});
-
-var TagCollection = Backbone.Collection.extend({
-    model: TagModel,
+/**
+ *
+ */
+BeagleWizard.TagCollection = Backbone.Collection.extend({
+    model: BeagleWizard.TagModel,
     url: '/t'
 });
 
-var TagView = Backbone.View.extend({
-    model: TagModel,
-    tagName: 'tr',
 
-    /**
-     * Deletes the tag represented by the model.
-     *
-     * @param {jQuery.Event} e Click event. Contains the model in data.
-     */
-    clickDelete: function(e) {
-        tagId = this.model.get('id');
-        $.ajax({
-            context: this,
-            type: 'DELETE',
-            url: `/t/${tagId}`,
-            error: function(request, status, err) {
-                console.error(`error deleting tag ${tagId}: ${err}`);
-            },
-            success: function(data, status, request) {
-                this.remove();
-            }
-        });
+/**
+ *
+ */
+BeagleWizard.TagView = Backbone.Marionette.ItemView.extend({
+    template: "#tag-template",
+    tagName: 'tr'
+});
+
+
+/**
+ *
+ */
+BeagleWizard.TagTableView = Backbone.Marionette.CompositeView.extend({
+    tagName: "table",
+    id: "tag-table",
+    className: 'is-narrow is-striped table',
+    template: "#tag-table-template",
+    itemView: BeagleWizard.TagView,
+
+    initialize: function(){
+        this.listenTo(this.collection, "sort", this.renderCollection);
     },
 
-    /**
-     * Renders the tag to the assigned element.
-     *
-     * @returns {TagView} This TagView.
-     */
-    render: function() {
-        let tagId = this.model.get('id');
-        let context = {
-            id: tagId,
-            label: this.model.get('label'),
-        };
-        let output = TagTemplate(context);
-        this.$el.html(output);
-        $(`#delete-${tagId}`).click(this, function (e) { e.data.clickDelete(e); });
-        return this;
+    appendHtml: function(collectionView, itemView){
+        collectionView.$('tbody').append(itemView.el);
     }
 });
 
-var TagListView = Backbone.View.extend({
-    model: TagCollection,
-
-    /**
-     * Renders the collection of tags.
-     *
-     * @returns {TagListView} This TagListView.
-     */
-    render: function() {
-        this.$el.html('');
-        for (tag of this.model) {
-            let tag_view = new TagView({model: tag});
-            this.$el.append(tag_view.$el);
-            tag_view.render();
-        }
-        return this;
-    },
-
-    /**
-     * Updates the list view by fetching the collection and rendering it.
-     */
-    update: function() {
-        this.model.fetch({
-            view: this,
-
-            /**
-             */
-            success: function(collection, response, options) {
-                options.view.render();
-            }
-        });
-    }
-});
 
 // perform logic that needs the DOM to have finished loading
-$(document).ready(function() {
-    var bookmarks = new BookmarkCollection();
-
-    var bookmarks_view = new BookmarkListView({
-        el: $('#bookmark-list'),
-        model: bookmarks
-    });
-
-    bookmarks_view.update();
-
-    var tags = new TagCollection();
-
-    var tags_view = new TagListView({
-        el: $('#tag-list'),
-        model: tags
-    });
-
-    tags_view.update();
-
+$(document).ready(function(){
     $('#bookmark-add').click(function(e) {
         $.ajax({
             contentType: "application/json; charset=utf-8",
@@ -253,8 +149,10 @@ $(document).ready(function() {
                 console.error(`error adding bookmark: ${err}`);
             },
             success: function(data, status, request) {
-                bookmarks.add(data, {at: 0});
-                bookmarks_view.render();
+                $('#bookmark-label').val('');
+                $('#bookmark-text').val('');
+                $('#bookmark-url').val('');
+                bookmarks.fetch();
             }
         });
     });
@@ -271,9 +169,21 @@ $(document).ready(function() {
                 console.error(`error adding tag: ${err}`);
             },
             success: function(data, status, request) {
-                tags_view.render();
+                $('#tag-label').val('');
+                tags.fetch();
             }
         });
+    });
+
+    let bookmarks = new BeagleWizard.BookmarkCollection();
+    bookmarks.fetch();
+
+    let tags = new BeagleWizard.TagCollection();
+    tags.fetch();
+
+    BeagleWizard.App.start({
+        bookmarks: bookmarks,
+        tags: tags
     });
 });
 
